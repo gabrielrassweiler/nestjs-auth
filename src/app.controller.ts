6ee621f3d694be9e2c9f1e983ba205c19bf79b5a
gabrielrassweiler <gabriel_rassweiler@hotmail.com>
2023-01-20 09:00:26 -0300

    feat(auth,middleware): implement login validation and decorator current user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
+import { User } from './user/entities/user.entity';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('me')
+  // CurrentUser é um decorator personalizado
+  getMe(@CurrentUser() user: User) {
+    return user;
   }
 }
